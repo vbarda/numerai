6970f4f3e93b9318241e08554cd63028026a2eb9
Vadym Barda <vadim.barda@gmail.com>
2016-09-17 17:57:04 -0400

    added function to display the most correlated variables

---
+++ tools/stats.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+
+def display_biggest_corrs(X, feature_names, cutoff=.8):
+    '''List variable pairs that are most correlated (above cutoff):
+    Args:
+        X: (df) of features
+        feature_names: (list) of features to select
+        cutoff: (float) cutoff for biggest correlations
+    '''
+    corrs = X[feature_names].corr()
+    filtered = corrs[(corrs != 1) & (corrs.abs() > cutoff)].stack().drop_duplicates()
+    for k, v in filtered.iteritems():
+        print '{}, {} -> {}'.format(k[0], k[1], v)
