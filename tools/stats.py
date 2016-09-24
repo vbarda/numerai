2ab2682326bbc496ee6b31a1d577c97896a934dd
Vadym Barda <vadim.barda@gmail.com>
2016-09-24 18:18:31 -0400

    better defaults for display_biggest_corrs

--- tools/stats.py
+++ tools/stats.py
@@ -1,13 +1,15 @@
 import pandas as pd
 
 
-def display_biggest_corrs(X, feature_names, cutoff=.8):
+def display_biggest_corrs(X, feature_names=None, cutoff=.8):
     '''List variable pairs that are most correlated (above cutoff):
     Args:
         X: (df) of features
         feature_names: (list) of features to select
         cutoff: (float) cutoff for biggest correlations
     '''
+    if feature_names is None:
+        feature_names = X.columns
     corrs = X[feature_names].corr()
     filtered = corrs[(corrs != 1) & (corrs.abs() > cutoff)].stack().drop_duplicates()
     for k, v in filtered.iteritems():
