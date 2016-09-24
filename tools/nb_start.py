9fee0d88861634d0d50d9b3de6fccd72b072bd93
Vadym Barda <vadim.barda@gmail.com>
2016-09-24 18:49:14 -0400

    added nb_start file that contains the most important functions

---
+++ tools/nb_start.py
@@ -0,0 +1,26 @@
+%matplotlib inline
+%load_ext autoreload
+%autoreload 2
+
+import sys
+sys.path.append('../../numerai/')
+
+import pandas as pd
+import numpy as np
+import funcy
+import os
+
+from sklearn.decomposition import PCA
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+from sklearn.feature_selection import RFECV, SelectKBest
+from sklearn.linear_model import LogisticRegression, LinearRegression, LassoCV, LassoLarsCV
+from sklearn.metrics import roc_auc_score, accuracy_score, f1_score
+from sklearn.pipeline import Pipeline, make_pipeline
+from sklearn.preprocessing import PolynomialFeatures, StandardScaler, binarize
+from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
+
+from models.cross_validation import CrossValidation
+from models.inspection import ModelInspector
+from tools.stats import display_biggest_corrs
+
+binarize = funcy.partial(binarize, threshold=.5)
