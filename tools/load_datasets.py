ade967d74785b45357d5c0b06057ff149e5e6c9d
Vadym <vadim.barda@gmail.com>
2017-01-30 20:36:23 -0500

    rewrite data ingest to account for new format (one zip archive)

--- tools/load_datasets.py
+++ tools/load_datasets.py
@@ -4,10 +4,10 @@ import pandas as pd
 import urllib2
 
 from paths import (EXISTING_TRAIN_DATA_PATH, EXISTING_TOURNAMENT_DATA_PATH,
-                   NEW_TRAIN_DATA_PATH, NEW_TOURNAMENT_DATA_PATH)
+                   NEW_TRAIN_DATA_PATH, NEW_TOURNAMENT_DATA_PATH, NEW_DATA_PATH)
 from tools.decorator_utils import check_against_cache
 from tools.path_utils import mkdir_if_not_there
-from tools.url_utils import safe_read_csv_from_url, filter_url_list, safe_url_open
+from tools.url_utils import  safe_unzip_from_url, safe_url_open
 
 
 DEFAULT_NUMERAI_URL = 'https://numer.ai/rules'
@@ -23,25 +23,34 @@ def _get_data_urls(url=DEFAULT_NUMERAI_URL, data_keyword=DEFAULT_NUMERAI_DATA_KE
     '''
     url_string = safe_url_open(url).read()
     soup = BeautifulSoup(url_string, "html.parser")
-    links = [tag.get('href') for tag in soup('a')]  # extract all links from the url page
-    # return only links which contain data_keyword
-    return [link for link in links if data_keyword in link]
+    # extract all links from the url page & return only links which contain data_keyword
+    links = filter(lambda x: data_keyword in x, [tag.get('href') for tag in soup('a')])
+    # keeping this in case they ever change it again, as it used to be two separate links
+    if len(links) == 1:
+        return links[0]
+    return links
+
+
+def refresh_data(path=NEW_DATA_PATH):
+    '''extract the train and test data from zip on the website and write to today's folder'''
+    mkdir_if_not_there(path)
+    safe_unzip_from_url(_get_data_urls(), path)
 
 
 @check_against_cache(EXISTING_TRAIN_DATA_PATH)
-def get_training_data(from_cache=False):
+def get_training_data(from_cache=True):
     '''Get Numerai training data as pd.DataFrame
     Args:
         from_cache: (bool) if from_cache is True, data will be loaded from EXISTING_TRAIN_DATA_PATH
     '''
     if from_cache:
         return pd.read_csv(EXISTING_TRAIN_DATA_PATH)
-    training_data_csv_url = filter_url_list(_get_data_urls(), 'training')
-    return safe_read_csv_from_url(training_data_csv_url)
+    refresh_data()
+    return pd.read_csv(NEW_TRAIN_DATA_PATH)
 
 
 @check_against_cache(EXISTING_TOURNAMENT_DATA_PATH)
-def get_tournament_data(from_cache=False):
+def get_tournament_data(from_cache=True):
     '''Get Numerai tournament data as pd.DataFrame
     Args:
         from_cache: (bool) if from_cache is True,
@@ -49,21 +58,10 @@ def get_tournament_data(from_cache=False):
     '''
     if from_cache:
         return pd.read_csv(EXISTING_TOURNAMENT_DATA_PATH)
-    tournament_data_csv_url = filter_url_list(_get_data_urls(), 'tournament')
-    return safe_read_csv_from_url(tournament_data_csv_url)
+    refresh_data()
+    return pd.read_csv(NEW_TOURNAMENT_DATA_PATH)
 
 
 def _save_data(getter, path):
     '''wrapper around data getters to save to csv'''
-    mkdir_if_not_there(path)
     return getter().to_csv(path, index=False)
-
-
-def save_training_data(path=NEW_TRAIN_DATA_PATH):
-    '''save training data'''
-    return _save_data(getter=get_training_data, path=path)
-
-
-def save_tournament_data(path=NEW_TOURNAMENT_DATA_PATH):
-    '''save tournament data'''
-    return _save_data(getter=get_tournament_data, path=path)
--- tools/url_utils.py
+++ tools/url_utils.py
@@ -1,6 +1,8 @@
 import urllib2
 import pandas as pd
 from StringIO import StringIO
+from zipfile import ZipFile
+import warnings
 
 
 def safe_url_open(url):
@@ -13,9 +15,13 @@ def safe_url_open(url):
 def safe_read_csv_from_url(csv_url):
     '''Read csv from url with safe_url_open, StringIO and pd.read_csv'''
     url_open = safe_url_open(csv_url)
+    with warnings.catch_warnings():
+        warnings.simplefilter('always')
+        warnings.warn('Use safe_unzip_from_url', DeprecationWarning)
     return pd.read_csv(StringIO(url_open.read()))
 
-
-def filter_url_list(url_list, filter_keyword):
-    '''Filter the url list and extract url that matches filter keyword'''
-    return [url for url in url_list if filter_keyword in url][0]
+def safe_unzip_from_url(zip_url, extract_path):
+    '''Read zip from url with safe_url_open, StringIO and unzip to extract_path'''
+    url_open = safe_url_open(zip_url)
+    with ZipFile(StringIO(url_open.read()), 'r') as f:
+        f.extractall(extract_path)
